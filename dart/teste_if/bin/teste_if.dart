90df84d56094047a77a36a08f5e4ededd6254c91
TiozinDoPicole <gustavo.fe.alves2016@gmail.com>
2021-08-30 15:06:38 -0300

    teste if

---
+++ dart/teste_if/bin/teste_if.dart
@@ -0,0 +1,13 @@
+import 'package:teste_if/teste_if.dart' as teste_if;
+import 'dart:io';
+
+void main(List<String> arguments) {
+  print('Digite sua nota: ');
+  var textonota = stdin.readLineSync() ?? "";
+  var nota = double.parse(textonota);
+  if (nota >= 6) {
+    print('Você passou!');
+  } else {
+    print('Você reprovou');
+  }
+}
---
+++ dart/teste_if/test/teste_if_test.dart
@@ -0,0 +1,8 @@
+import 'package:teste_if/teste_if.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
